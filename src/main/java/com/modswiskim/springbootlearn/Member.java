4d36544e763dc2148312b8e5f63a9beb71364534
Kimhy-git <modsiwkim@gmail.com>
2023-11-07 20:55:10 +0900

    entity 관련

--- src/main/java/com/modswiskim/springbootlearn/Member.java
+++ src/main/java/com/modswiskim/springbootlearn/Member.java
@@ -6,13 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 기본 생성자
 @AllArgsConstructor
 @Getter
-@Entity
+@Entity(name = "member_list")
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id // id 필드를 기본값으로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
     @Column(name = "id", updatable = false)
     private Long id;    // DB table의 'id'와 매칭
 
