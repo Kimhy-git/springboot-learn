dd0729c36c61469bfcf4a8f9f31e2874e09753d6
Kimhy-git <modsiwkim@gmail.com>
2023-11-09 14:16:56 +0900

    blog 만들기 - CRUD

--- src/main/java/com/modswiskim/springbootlearn/service/BlogService.java
+++ src/main/java/com/modswiskim/springbootlearn/service/BlogService.java
@@ -2,9 +2,13 @@ package com.modswiskim.springbootlearn.service;
 
 import com.modswiskim.springbootlearn.domain.Article;
 import com.modswiskim.springbootlearn.dto.AddArticleRequest;
+import com.modswiskim.springbootlearn.dto.UpdateArticleRequest;
 import com.modswiskim.springbootlearn.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor    // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service
@@ -12,8 +16,35 @@ public class BlogService {
 
     private final BlogRepository blogRepository;
 
-    // 블로그 글 추가 메서드
+    // 블로그 글 추가
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // 블로그 글 전체 조회
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    // 블로그 글 단일 조회
+    public Article findById(long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: "+id)); // id를 받아 엔티티를 조회하고 없으면 IllegalArgumentException 예외를 발생시킨다.
+    }
+
+    // 블로그 글 삭제
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
+
+    // 블로그 글 수정
+    @Transactional  // 매칭한 메서드를 하나의 트랜잭션으로 묶는 역할 (트랜잭션 : 데이터베이스의 데이터를 바꾸기 위한 단위 작업) 엔티티의 필드 값이 바뀌면 중간에 에러가 발생해도 제대로된 값 수정을 보장한다.
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: "+id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
     }
 }
--- src/test/java/com/modswiskim/springbootlearn/controller/BlogApiControllerTest.java
+++ src/test/java/com/modswiskim/springbootlearn/controller/BlogApiControllerTest.java
@@ -3,6 +3,7 @@ package com.modswiskim.springbootlearn.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.modswiskim.springbootlearn.domain.Article;
 import com.modswiskim.springbootlearn.dto.AddArticleRequest;
+import com.modswiskim.springbootlearn.dto.UpdateArticleRequest;
 import com.modswiskim.springbootlearn.repository.BlogRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -20,7 +21,8 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest     // 테스트용 어플리케이션 컨텍스트
@@ -72,5 +74,107 @@ class BlogApiControllerTest {
         assertThat(articles.size()).isEqualTo(1);
         assertThat(articles.get(0).getTitle()).isEqualTo(title);
         assertThat(articles.get(0).getContent()).isEqualTo(content);
+    }
+
+    @DisplayName("findAllArticles : 블로그 글 목록 조회에 성공한다.")
+    @Test
+    public void findAllArticles() throws Exception {
+        // given
+        final String url = "/api/articles";
+        final String title = "title";
+        final String content = "content";
+
+        blogRepository.save(Article.builder()
+                        .title(title)
+                        .content(content)
+                        .build());
+
+        // when
+        final ResultActions resultActions = mockMvc.perform(get(url)
+                .accept(MediaType.APPLICATION_JSON));
+
+        // then
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].content").value(content))
+                .andExpect(jsonPath("$[0].title").value(title));
+    }
+
+    @DisplayName("findArticle : 블로그 글 조회에 성공한다.")
+    @Test
+    public void findArticle() throws Exception {
+        // given : 블로그 글을 저장합니다
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+
+        Article savedArticle = blogRepository.save(Article.builder()
+                        .title(title)
+                        .content(content)
+                        .build());
+
+        // when : 저장한 블로그 글의 id 값으로 api를 호출합니다
+        final ResultActions resultActions = mockMvc.perform(get(url, savedArticle.getId()));
+
+        // then : 응답 코드가 200 ok이고, 반환받은 content와 title이 일치하는지 확인합니다
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value(content))
+                .andExpect(jsonPath("$.title").value(title));
+    }
+
+    @DisplayName("deleteArticle : 블로그 글 삭제에 성공한다.")
+    @Test
+    public void deleteArticle() throws Exception {
+        // given : 블로그 글을 저장합니다
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        // when : 저장한 블로그 글의 id 값으로 삭제 api를 호출합니다
+        mockMvc.perform(delete(url, savedArticle.getId()))
+                        .andExpect(status().isOk());
+
+        // then : 응답 코드가 200 ok이고, 블로그 글 전체 조회해 배열 크기가 0인지 확인합니다
+        List<Article> articles = blogRepository.findAll();
+
+        assertThat(articles).isEmpty();
+    }
+
+    @DisplayName("updateArticle : 블로그 글 수정에 성공한다")
+    @Test
+    public void updateArticle() throws Exception {
+        // given : 블로그 글을 저장하고 글 수정에 필요한 요청 객체를 만든다
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+
+        Article savedArticle = blogRepository.save(Article.builder()
+                        .title(title)
+                        .content(content)
+                        .build());
+
+        final String newTitle = "new title";
+        final String newContent = "new content";
+
+        UpdateArticleRequest request = new UpdateArticleRequest(newTitle, newContent);
+
+        // when : update api로 수정 요청을 보낸다. 요청 타입은 json이며, given절에서 미리 만들어둔 객체를 요청 본문으로 함께 보낸다
+        ResultActions result = mockMvc.perform(put(url, savedArticle.getId())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(request)));
+
+        // then
+        result.andExpect(status().isOk());
+
+        Article article = blogRepository.findById(savedArticle.getId()).get();
+
+        assertThat(article.getTitle()).isEqualTo(newTitle);
+        assertThat(article.getContent()).isEqualTo(newContent);
     }
 }
