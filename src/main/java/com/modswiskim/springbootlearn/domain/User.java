58acb929aac8ed984eaf817602f28441f5c39942
Kimhy-git <modsiwkim@gmail.com>
2023-11-14 21:08:10 +0900

    spring security - login구현

--- src/main/java/com/modswiskim/springbootlearn/domain/User.java
+++ src/main/java/com/modswiskim/springbootlearn/domain/User.java
@@ -4,12 +4,18 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "users")
 @NoArgsConstructor
 @Getter
 @Entity
-public class User {
+public class User implements UserDetails {  // UserDetails를 상속받아 인증 객체로 사용
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,6 +32,49 @@ public class User {
     public User(String email, String password, String auth) {
         this.email = email;
         this.password = password;
+    }
+
+    // 권한 반환 : 사용자가 가지고 있는 권한의 목록을 반환합니다. 현재 예제코드에서는 사용자 이외의 권환이 없기 때문에 user 권한만 담아 반환합니다.
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    // 사용자의 id를 반환(고유한 값)
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    // 사용자의 password 반환
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    // 계정 만료 여부 반환
+    @Override
+    public boolean isAccountNonExpired() {
+        // 만료되었는지 확인하는 로직
+        return true;    // true : 만료되지 않았음
+    }
+
+    // 계정 잠금 여부 반환
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // 패스워드의 만료 여부 반환
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // 계정 사용 가능 여부 반환
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
 }
---
+++ src/main/java/com/modswiskim/springbootlearn/service/UserService.java
@@ -0,0 +1,24 @@
+package com.modswiskim.springbootlearn.service;
+
+import com.modswiskim.springbootlearn.domain.User;
+import com.modswiskim.springbootlearn.dto.AddUserRequest;
+import com.modswiskim.springbootlearn.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                // 패스워드 암호화
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
